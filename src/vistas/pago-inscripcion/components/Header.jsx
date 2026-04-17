87f432b43ee4da326d1b49ad0420302c5d27aac7
Daniel Salcedo <johandanielgs@ufps.edu.co>
2026-04-17 15:30:38 -0500

    feat: actualizado componente Header para incluir información basica de la pagina

--- src/vistas/pago-inscripcion/components/Header.jsx
+++ src/vistas/pago-inscripcion/components/Header.jsx
@@ -1,8 +1,15 @@
 function Header() {
   return (
-    <header>
-      <h1>Pago de Inscripción</h1>
-    </header>
+    <div className="bg-red-700 text-white px-6 py-4 flex justify-between">
+      <div>
+        <h1 className="font-bold">UFPS</h1>
+        <p className="text-sm">Postgrados</p>
+      </div>
+
+      <div className="flex items-center gap-2">
+        <span>Juan Pérez</span>
+      </div>
+    </div>
   );
 }
 
