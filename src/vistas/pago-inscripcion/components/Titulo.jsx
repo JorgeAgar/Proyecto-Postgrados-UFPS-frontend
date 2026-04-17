86deeb26b47e69a09ba8c55bb8fe56a48fc1352f
Daniel Salcedo <johandanielgs@ufps.edu.co>
2026-04-17 15:39:20 -0500

    feat: añadir componente Titulo para la página de pago de inscripción

---
+++ src/vistas/pago-inscripcion/components/Titulo.jsx
@@ -0,0 +1,12 @@
+function Titulo() {
+  return (
+      <div>
+      <h2 className="text-3xl font-bold">Pago de Inscripción</h2>
+      <p className="text-gray-600">
+        Consulta la información de tu inscripción, genera tu recibo y paga en línea.
+      </p>
+    </div>
+  );
+}
+
+export default Titulo;
--- src/vistas/pago-inscripcion/pages/PagoInscripcionPage.jsx
+++ src/vistas/pago-inscripcion/pages/PagoInscripcionPage.jsx
@@ -1,9 +1,13 @@
 import Header from "../components/Header";
-
+import Titulo from "../components/Titulo";
 function PagoInscripcionPage(){
 
     return( <div className="min-h-screen bg-gray-100">
       <Header />
+      
+      <main className="max-w-6xl mx-auto p-6 space-y-6">
+      <Titulo />
+      </main>
     </div>
   );
 }
