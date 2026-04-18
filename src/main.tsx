4515fe305f32baa796ba73385db8558b6a73547a
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2026-04-17 19:40:23 -0500

    Aviso de clave valida o no

--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,16 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import Status from "./vistas/Status.tsx";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Route, Routes } from "react-router";
+import { Formulario } from "./vistas/FormInscripcion.tsx";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
-      <Status />
+    <Routes>
+      <Route path="/" element={<Status />} />
+      <Route path="/inscripcion" element={<Formulario />} />
+    </Routes>
     </BrowserRouter>
   </StrictMode>,
 );
