20bc8e75a496372cf536eb187c45368bbe5520d8
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2026-05-23 20:39:00 -0500

    Añadido login de posgrados

---
+++ src/services/posgrados/posgradosLoginService.ts
@@ -0,0 +1,46 @@
+
+/**
+ * Manda datos al backend y guarda la vaina esa que devuelve el backend en una cookie
+ * @param username el username para logearse
+ * @param password la contraseña para logearse
+ * @returns si se logeo correctamente o no
+ */
+export const logearFacultad = async (username: string, password: string) => {
+  const response = await fetch(`${import.meta.env.VITE_API_URL}/auth/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      username,
+      password,
+      requestedRole: "Posgrados",
+    }),
+  });
+
+  console.log(
+    "Enviando solicitud de login: ",
+    JSON.stringify({ username, password, requestedRole: "Posgrados" }),
+  );
+
+  if (!response.ok) {
+    const errorData = await response.json();
+    const errorMessage =
+      errorData?.message || "Error desconocido al iniciar sesión.";
+    console.error("Error en la respuesta del login:", errorMessage);
+    console.error(response.status, response.statusText);
+    throw new Error(errorMessage);
+  }
+
+  try {
+    const loginResponse = await response.json();
+    // console.log(loginResponse);
+    const cookieValue = encodeURIComponent(JSON.stringify(loginResponse));
+    // Guarda cookie de sesión para la autenticación
+    document.cookie = `auth=${cookieValue}; path=/`;
+  } catch (err) {
+    console.error("Error parseando la respuesta del login:", err);
+    throw err;
+  }
+};
+
