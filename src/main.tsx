1f382f00adf1410e1a418c29ecb9e1fa16acfd81
DanielGSalcedo <johadanielgs@ufps.edu.co>
2026-05-28 01:14:53 -0500

    fix: remove EditarCohorte route from main application routing

--- src/main.tsx
+++ src/main.tsx
@@ -41,7 +41,6 @@ import Calificacion from "./vistas/programa/calificacion/Calificacion.tsx";
 import CalificacionCohorte from "./vistas/programa/calificacion/CalificacionCohorte.tsx";
 import CalificacionAspirante from "./vistas/programa/calificacion/CalificacionAspirante.tsx";
 import ProgramaAdmitidos from "./vistas/programa/ProgramaAdmitidos.tsx";
-import EditarCohorte from "./vistas/programa/cohorte/EditarCohorte.tsx";
 
 // Vistas de posgrados
 import Posgrados from "./vistas/posgrados/Posgrados.tsx";
@@ -97,7 +96,6 @@ createRoot(document.getElementById("root")!).render(
           <Route path="crear-cohorte" element={<CrearCohorte />} />
           <Route path="cohortes" element={<Cohortes />} />
           <Route path="documentos" element={<ProgramaDocumentos />} />
-          <Route path="editar-cohorte/:id" element={<EditarCohorte />} />
           <Route path="criterios" element={<Criterios />} />
           <Route path="validacion" element={<ValidacionDocumentos />} />
           <Route path="validacion/cohorte/:cohorteId" element={<ValidacionCohorteDetalle />} />
