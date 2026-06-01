64e3e35f267361e9b2f9b40c65a32febd0a64460
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2026-06-01 00:01:55 -0500

    actualizar icono de la sidebar

--- src/vistas/superadmin/components/Sidebar.tsx
+++ src/vistas/superadmin/components/Sidebar.tsx
@@ -134,20 +134,9 @@ function valoresIcon() {
 
 function documentsIcon() {
   return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-      strokeWidth={1.5}
-      stroke="currentColor"
-      className="size-6"
-    >
-      <path
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        d="M19.5 7.5V6a2.25 2.25 0 0 0-2.25-2.25H6.75A2.25 2.25 0 0 0 4.5 6v12A2.25 2.25 0 0 0 6.75 20.25h10.5A2.25 2.25 0 0 0 19.5 18v-1.5m0-9h-7.5m7.5 0L16.5 4.5M19.5 7.5 16.5 10.5m0 0H9m7.5 0L13.5 7.5M9 10.5H6.75"
-      />
-    </svg>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 14.25v-2.625a3.375 3.375 0 0 0-3.375-3.375h-1.5A1.125 1.125 0 0 1 13.5 7.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 0 0-9-9Z" />
+</svg>
   );
 }
 
