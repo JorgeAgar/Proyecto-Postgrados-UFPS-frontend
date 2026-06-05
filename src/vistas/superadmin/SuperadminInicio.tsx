39cdcdbe63dfaaa13f99c2cb4059331fead63e1f
Jorge Andrés Reyes Serrano <111466784+JorgeAgar@users.noreply.github.com>
2026-06-05 17:29:34 -0500

    arreglar icono de ultimos codiogs en inicio de superadmin

--- src/vistas/superadmin/SuperadminInicio.tsx
+++ src/vistas/superadmin/SuperadminInicio.tsx
@@ -42,9 +42,10 @@ function DocumentIcon() {
 
 function HashtagIcon() {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" className="w-7 h-7 shrink-0" stroke="currentColor" strokeWidth="1.5">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5-3.9 19.5m-4.2-19.5-3.9 19.5" />
-    </svg>
+     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M5.25 8.25h15m-16.5 7.5h15m-1.8-13.5-3.9 19.5m-2.1-19.5-3.9 19.5" />
+</svg>
+
   );
 }
 
